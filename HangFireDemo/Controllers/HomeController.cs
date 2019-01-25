df704c21afc101c25641350c9bc255f4302ef104
Paul Brodzinski <pbrodz@hotmail.com>
2019-01-25 15:28:44 -0800

    Initial Commit

---
+++ HangFireDemo/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Hangfire;
+using HangFireDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Threading;
+
+namespace HangFireDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            BackgroundJob.Enqueue(() => LongRunningMethod());
+            BackgroundJob.Enqueue(() => CallLocalSqlServer());
+            return View();
+        }
+
+        public void LongRunningMethod()
+        {
+            Thread.Sleep(5000);
+        }
+
+        public void CallLocalSqlServer()
+        {
+            using (var connection = new SqlConnection("Server=.;Database=BetCoreDb;Trusted_Connection=True;MultipleActiveResultSets=true"))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("WAITFOR DELAY '0:00:10'", connection)
+                {
+                    CommandType = CommandType.Text
+                };
+                DbCommand cmd = command;
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HangFireDemo/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HangFireDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddHangfire(config => config.UseSqlServerStorage(Configuration.GetConnectionString("HangfireDB")));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
